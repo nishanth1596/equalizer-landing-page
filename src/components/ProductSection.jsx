5fa80f0586737bde64a0050b830446659e250a60
Nishanth <nishanth1596@gmail.com>
2025-01-20 08:38:56 +0530

    given more accurate margin to the product section

--- src/components/ProductSection.jsx
+++ src/components/ProductSection.jsx
@@ -5,7 +5,7 @@ import Card from "./Card";
 const ProductSection = () => {
   return (
     <section>
-      <div className="relative mt-44 w-full bg-slate pt-96 md:max-w-[42.5rem] md:rounded-xl md:pt-[37.5rem] xl:max-w-[72rem]">
+      <div className="relative mt-44 w-full bg-slate pt-96 md:max-w-[42.5rem] md:rounded-xl md:pt-[37.5rem] xl:mt-[16.5rem] xl:max-w-[72rem]">
         <img
           className="absolute left-1/2 top-[-6.875rem] z-20 max-w-52 -translate-x-1/2 md:left-11 md:max-w-[16.89rem] md:translate-x-0 xl:left-[6.25rem] xl:max-w-[19.5rem] xl:translate-x-0"
           src={illustrationMobileImg}
